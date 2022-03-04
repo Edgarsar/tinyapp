c93c065c6e2868ce2d0c3dc9fc8c5d3eee3620ed
Edgarsar <edgarsargsyan@hotmail.com>
2022-03-04 01:27:32 +0000

    Add a new route to render the urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -8,10 +8,14 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// use res.render to load up an ejs view file
+// use res.render to load up a "urls_index.ejs" view file
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+// use res.render to load up an "urls_new.ejs" view file
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -19,7 +23,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// respond with "Hello!" when a GET request is made to the homepage
+/* // respond with "Hello!" when a GET request is made to the homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -32,7 +36,7 @@ app.get("/urls.json", (req, res) => {
 // respond with "html code" when a GET request is made to the homepage
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+}); */
 
 
 app.listen(PORT, () => {
