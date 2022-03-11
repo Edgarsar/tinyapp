3e21b621d74d7cdc25cc7ff4e21e3f9be2ddc6a9
Edgarsar <edgarsargsyan@hotmail.com>
2022-03-11 20:44:22 +0000

    Require the functions from helpers.js

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 const PORT = 8080; // default port 8080
+const { getUserByEmail, generateRandomString, urlsForUser } = require('./helpers')
 
 app.set("view engine", "ejs");
 
@@ -46,39 +47,6 @@ const users = {
   }
 };
 
-// Email lookup function
-const getUserByEmail = (email, database) => {
-  for (const userId in database) {
-    const user = database[userId];
-    if (user.email === email) {
-      return user;
-    }
-  }
-  return null;
-};
-
-// Generates a string of 6 random alphanumeric characters
-const generateRandomString = () => {
-  let randomString = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = 6;
-  for (let i = 0; i < length; i++) {
-    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return randomString;
-};
-
-
-// Returns the URLs where the userID is equal to the id of the currently logged-in user
-const urlsForUser = (id, database) => {
-  const newObj = {};
-  for (const urlId in database) {
-    if (database[urlId]["userID"] === id) {
-      newObj[urlId] = database[urlId];
-    }
-  }
-  return newObj;
-};
 
 // Use res.render to load up a "urls_index.ejs" view file
 app.get("/urls", (req, res) => {
